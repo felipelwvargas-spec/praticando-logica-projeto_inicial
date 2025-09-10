fd8e9cf6688ed2cab7aca609f4e38bd5504e07a5
Felipe Vargas <felipelwvargas@gmail.com>
2025-09-10 16:34:50 -0300

    compra de ingresso

---
+++ ingresso/js/app.js
@@ -0,0 +1,15 @@
+function comprar(){
+    let tipoIngresso = document.getElementById('tipo-ingresso').value
+    let qtd = document.getElementById('qtd').value
+    let qtdPista = document.getElementById('qtd-pista').textContent
+    let qtdSuperior = document.getElementById('qtd-superior').textContent
+    let qtdinferior = document.getElementById('qtd-inferior').textContent
+    
+
+
+    console.log(tipoIngresso);
+    console.log(qtd);
+    console.log(qtdPista);
+    console.log(qtdSuperior);
+    console.log(qtdinferior);
+}
