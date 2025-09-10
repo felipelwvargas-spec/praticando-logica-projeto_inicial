d308e6a4ba26d1df2069fcbd0671bc74de835071
Felipe Vargas <felipelwvargas@gmail.com>
2025-09-10 08:34:24 -0300

    Solicitar confirmação

--- alugames/js/app.js
+++ alugames/js/app.js
@@ -2,18 +2,25 @@ function alterarStatus(id) {
     let gameClicado = document.getElementById(`game-${id}`);
     let imagem = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
-    // let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
+    let r = confirm("Deseja continuar?");
+    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
 
-    if (imagem.classList.contains('dashboard__item__img--rented')) {
-        imagem.classList.remove('dashboard__item__img--rented');
-        botao.classList.remove('dashboard__item__button--return');
-        botao.textContent = 'Alugar';
-    } else {
-        imagem.classList.add('dashboard__item__img--rented');
-        botao.classList.add('dashboard__item__button--return');
-        botao.textContent = 'Devolver';
+    if (r == true) {
+        if (imagem.classList.contains('dashboard__item__img--rented')) {
+            imagem.classList.remove('dashboard__item__img--rented');
+            botao.classList.remove('dashboard__item__button--return');
+            botao.textContent = 'Alugar';
+        } else {
+            imagem.classList.add('dashboard__item__img--rented');
+            botao.classList.add('dashboard__item__button--return');
+            botao.textContent = 'Devolver';
+        }
     }
+    else{
+        let x = "Você pressionou Cancelar!";
+    }
+
+
 
     // alert(nomeJogo.textContent);
 }
-
