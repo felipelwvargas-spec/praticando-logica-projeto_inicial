5aaf97a11140b80ab69a7af241df43388ff0bd63
Felipe Vargas <felipelwvargas@gmail.com>
2025-09-10 14:51:24 -0300

    carrinho de compras

---
+++ carrinho-compras/js/app.js
@@ -0,0 +1,12 @@
+function adicionar() {
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let produto = document.getElementById('produto').value;
+
+    
+    console.log(produto);
+    console.log(quantidade);
+}
+
+function limpar() {
+
+}
