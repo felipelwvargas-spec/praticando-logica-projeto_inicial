69cccaf323c7ad2ceb485aa01a67304919214d2d
Felipe Vargas <felipelwvargas@gmail.com>
2025-09-10 16:12:50 -0300

    criado função para incluir item e limpar

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,12 +1,34 @@
-function adicionar() {
-    let quantidade = parseInt(document.getElementById('quantidade').value);
-    let produto = document.getElementById('produto').value;
+let total = 0;
+document.getElementById('lista-produtos').innerHTML = '';
+document.getElementById('valor-total').innerHTML = 'R$ 0';
+document.getElementById('quantidade').value = 1;
 
-    
-    console.log(produto);
-    console.log(quantidade);
+function adicionar() {
+    let produto = document.getElementById('produto').value;
+    let quantidade = document.getElementById('quantidade').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = produto.split('R$')[1];
+    let preco = quantidade * valorUnitario;
+    let carrinho = document.getElementById('lista-produtos');
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R${preco}</span>
+  </section>`;
+    total = total + preco;
+    let valorTotal = document.getElementById('valor-total');
+    valorTotal.textContent = `R$ ${total}`;
+    document.getElementById('quantidade').value = 1;
+
+    //console.log(produto);
+    //console.log(quantidade);
+    //console.log(nomeProduto);
+    //console.log(valorUnitario);
+    //console.log(preco);
+
 }
 
 function limpar() {
-
+    total = 0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').innerHTML = 'R$ 0';
+    document.getElementById('quantidade').value = 1;
 }
